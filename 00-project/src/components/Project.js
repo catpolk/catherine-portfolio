60954ee9b06ea752432aff9b8a63020a6bc6f214
Catherine <cpolknytx@gmail.com>
2022-12-20 15:27:57 -0600

    added portfolio page, added projects and links to github

--- 00-project/src/components/Project.js
+++ 00-project/src/components/Project.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Project(props) {
     return (
         <div>
-            <h1>Project {props.title}</h1>
-            <p> Stack {props.stack}</p>
+            <h1><a href={`${props.href}`}>Project: {props.title} </a></h1>
+            <p><a href={`${props.deployed}`}> Stack: {props.stack} </a></p>
             <div> This is description of project {props.title}</div>
         </div>
     )
--- 00-project/src/components/pages/Portfolio.js
+++ 00-project/src/components/pages/Portfolio.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Project from '../Project';
 
-const projects = [
-    { title: 'React', stack: 'MERN' },
-    { title: 'Express', stack: 'MERN' },
+const applications = [
+    { title: 'Social Network API', href: 'https://github.com/catpolk/Social-Network-API', stack: 'NoSQL', deployed: 'https://github.com/catpolk/world-connect-social-network' },
+    { title: 'Employee Tracking App', href: 'https://github.com/catpolk/employee-tracking-application', stack: 'SQL' },
+    { title: 'Note Taking App', href: 'https://github.com/catpolk/note-taking-application', stack: 'Express' },
+    { title: 'My Team Profile Generator', href: 'https://github.com/catpolk/my-team-profile-generator', stack: 'OOP' },
+    { title: 'README Generator', href: 'https://github.com/catpolk/README-generator', stack: 'Node.js' },
+    { title: 'The Arrays', href: 'https://github.com/catpolk/project-2-The-Arrays', stack: 'MVC, Charts.js' },
 ]
 
 function Portfolio () {
-    const projectsList = projects.map((project) => { 
-        return <Project title={project.title} stack={project.stack} />
+    const projectsList = applications.map((project) => { 
+        return <Project 
+            title={project.title} 
+            stack={project.stack} 
+            href={project.href}
+            deployed={project.deployed}
+        />
     })
 
     return (
