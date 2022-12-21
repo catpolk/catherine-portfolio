557837bdfa8a03d3104ac0cb8e6a50ed5493ddd7
Catherine <cpolknytx@gmail.com>
2022-12-21 17:27:09 -0600

    added name and email to contact form and validation

--- 00-project/src/components/pages/Contact.js
+++ 00-project/src/components/pages/Contact.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers'
 
 function Contact () {
+    // const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    // const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+    // // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+        if (inputType === 'name') {
+    //         setName(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue);
+            if (!validateEmail(email)) {
+                setErrorMessage('Email or is invalid');    
+            } else { 
+                setErrorMessage('');
+            }
+        // } else {
+    //         setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault(email);
+    }
+
     return (
-        <h1>Contact</h1>
-    )
+        <div>
+          <form className="form">
+            <input
+            //   value={email}
+              name="name"
+              
+              type="text" 
+            />
+            <input
+            //   value={userName}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+            />
+            <textarea
+            //   value={password}
+              name="message"
+            />
+            <button type="button" onClick={handleFormSubmit}>Submit</button>
+          </form>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+        </div>
+      );
 }
 
 export default Contact;
