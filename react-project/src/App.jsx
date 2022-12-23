cd03d8d6bd533adc6b0b5d50e5168bfa403b52e6
Catherine <cpolknytx@gmail.com>
2022-12-23 12:54:26 -0600

    added links to the footer component

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import PagesContainer from './components/pages/PagesContainer';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
@@ -13,7 +13,7 @@ function App() {
       <PagesContainer />
 
 
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- react-project/src/components/Footer.js
+++ react-project/src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './styles/Footer.css';
 
 function Footer () {
     return (
-        <h1>Footer</h1>
+        <section>
+            <ul> 
+                <li><a href="https://github.com/catpolk" target="_blank"> Github</a></li>
+                <li><a href="https://www.linkedin.com/in/catherine-polk-860614178/" target="_blank">LinkedIn</a></li>
+                <li><a href="https://stackoverflow.com/users/19369839/catherine" target="_blank">LinkedIn</a></li>
+            </ul>
+        </section>
     )
 }
 
